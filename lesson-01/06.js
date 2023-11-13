05580027bb9630e5bd1d3322152b6bc1b8176470
Nikivos <nikivo1992@gmail.com>
2023-11-13 19:10:57 +0100

    06

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,3 +1,3 @@
 let str = "zzzzyzz";
-let result = str.includes("y") ? "нашел" : "заблудился";
-console.log(result);
+//let result = str.includes("y") ? "нашел" : "заблудился";
+//console.log(result);
